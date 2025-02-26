13b164de2d5a0b2485b2acb253596d97b37ff65b
Pradeep8253 <89512667+Pradeep8253@users.noreply.github.com>
2025-02-26 17:03:45 +0530

    ?topheader

--- src/components/Header/Topheader.jsx
+++ src/components/Header/Topheader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaMapMarkerAlt, FaBullhorn } from "react-icons/fa";
+import { FaBullhorn } from "react-icons/fa";
 import Marquee from "react-fast-marquee";
 import { useEffect, useState } from "react";
 import axios from "axios";
@@ -13,7 +13,7 @@ const TopHeader = () => {
     axios
       .get(`${import.meta.env.VITE_BACKEND_URL}/api/admin/get/announcements`)
       .then((response) => {
-        setAnnouncements(response.data);
+        setAnnouncements(response.data || []); // Ensure it's an array
         setLoading(false);
       })
       .catch((error) => {
@@ -26,15 +26,6 @@ const TopHeader = () => {
   return (
     <header className="bg-[#07294d] text-white p-2">
       <div className="flex flex-col md:flex-row items-center justify-center space-y-2 md:space-y-0 md:space-x-8 px-4 text-center">
-        {/* Address Section */}
-        <div className="flex items-center space-x-2 text-sm md:text-base">
-          {/* <FaMapMarkerAlt className="h-5 w-5 text-red-400" /> */}
-          {/* <div className="p-1">
-            226, Sai Enclave, Nandgram, Opp. KM Residency, Raj Nagar Extn,
-            Ghaziabad-201003
-          </div> */}
-        </div>
-
         {/* Announcement Section */}
         <div className="flex items-center space-x-2 text-sm md:text-base">
           <FaBullhorn className="h-5 w-5 text-yellow-400" />
@@ -45,9 +36,9 @@ const TopHeader = () => {
               <span>{error}</span>
             ) : (
               <Marquee speed={50} gradient={false} pauseOnHover={true}>
-                {announcements.map((announcement, index) => (
+                {announcements?.map?.((announcement, index) => (
                   <span key={index}>
-                    {announcement.text} &nbsp;&nbsp;&nbsp;
+                    {announcement?.text} &nbsp;&nbsp;&nbsp;
                   </span>
                 ))}
               </Marquee>
