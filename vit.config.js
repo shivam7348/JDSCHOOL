66a28674d1c62778b6af48a131b595718156a03b
Pradeep8253 <89512667+Pradeep8253@users.noreply.github.com>
2025-02-26 15:59:55 +0530

    updated vite

--- vit.config.js
+++ vit.config.js
@@ -4,6 +4,10 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    historyApiFallback: true,
+    open: true, // Server start hone ke baad browser me khul jayega
   },
+  build: {
+    outDir: "dist", // Default build output directory
+  },
+  base: "/", // Agar subdirectory me deploy karna ho to isko update karo
 });
